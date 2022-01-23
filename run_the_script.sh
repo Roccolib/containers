be18d225b571e12ab9d035083b695f8aa22eed80
Roccolib <87078870+Roccolib@users.noreply.github.com>
2022-01-23 12:16:38 +0200

    Delete run_the_script.sh

--- run_the_script.sh
+++
@@ -1,5 +0,0 @@
-for i in $(seq 0 2); do 
-  kubectl -n tls-kafka cp ./sources.list "kafkaclient-$i:/etc/apt/sources.list"
-  kubectl -n tls-kafka cp ./setup_ssl.sh "kafkaclient-$i:/opt/kafka/setup_ssl.sh"
-  kubectl -n tls-kafka exec -it "kafkaclient-$i" -- bash setup_ssl.sh
-done
